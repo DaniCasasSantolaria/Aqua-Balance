afa14a9e73b863f408cd67a4e57f8f8a8296c644
Daniel Casas <daniel.casas@enti.cat>
2024-10-23 12:38:24 +0200

    Falta el salto

--- Assets/Project/Player/HorizontalMovement.cs
+++ Assets/Project/Player/HorizontalMovement.cs
@@ -6,15 +6,41 @@ public class HorizontalMovement : MonoBehaviour{
 
     [SerializeField] private float initialX;
     [SerializeField] private float initialY;
-    private Transform player;
+
+    [SerializeField] private float speed;
+    private float horizontal;
+    bool isFacingRight;
+
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start() {
-        player = GetComponent<Transform>();
-        player.transform.position = new Vector2(initialX, initialY);
+        rb = GetComponent<Rigidbody2D>();
+        transform.position = new Vector2(initialX, initialY);
+        isFacingRight = true;
     }
 
     // Update is called once per frame
     void Update() {
-        
+        GetInput();
+        Move();
+    }
+
+    public void Move() {
+        Flip();
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private void GetInput() {
+        horizontal = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void Flip() {
+        if(isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f) {
+            isFacingRight = !isFacingRight;
+            Vector2 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 }
--- Assets/Project/Player/Jump.cs
+++ Assets/Project/Player/Jump.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
+
+    [SerializeField] private float jumpingPower = 16f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _Jump();
+    }
+
+    private void _Jump() {
+        if (Input.GetKey(KeyCode.Space) && isGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);   
+        }
+        if (Input.GetKey(KeyCode.Space) && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+
+    private bool isGrounded() {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 }
